58980e4ad184222ea56674716fdd6f07319c31e5
CaptainJack <captain_jack_mail@163.com>
2022-12-14 17:21:40 +0800

    ts & esmodule lerna config

---
+++ packages/utils/__tests__/utils.test.js
@@ -0,0 +1,5 @@
+import utils from '../src/utils.js';
+import { strict as assert } from 'assert';
+
+assert.strictEqual(utils(), 'Hello from utils');
+console.info("utils tests passed");
---
+++ packages/utils/dist/utils.js
@@ -0,0 +1,5 @@
+export default function utils() {
+    console.log("this is utils package");
+}
+utils();
+//# sourceMappingURL=utils.js.map
